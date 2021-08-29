63373876ed2db1fa700aa35b41c318c4e6a00b7d
Maryna <marynaskyba@gmail.com>
2021-08-29 19:10:08 +0300

    add  corrections

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import s from './Statistics.module.css';
-// import getRandom from './randomcolor';
+
 
 export default function Statistic({ title, stats }) {
   return (
